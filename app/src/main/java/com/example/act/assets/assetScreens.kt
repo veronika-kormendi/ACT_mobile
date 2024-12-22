d8ad604accd0d548331c5a1557e09079ca60d6ba
ChoFongCit <cho.leong@mycit.ie>
2024-12-22 13:56:58 +0000

    changed some variable names

---
+++ app/src/main/java/com/example/act/assets/assetScreens.kt
@@ -0,0 +1,25 @@
+package com.example.act.assets
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun AssetScreen(navController: NavController){
+    Box(){
+        Row(){
+
+        }
+    }
+}
+
+@Composable
+fun ToStock(){
+
+}
+
+@Composable
+fun ToCrypto(){
+
+}
